483455d3c6764d6678f2a7f064dd8290e8004e0e
starryeye <see2818@naver.com>
2023-09-05 02:27:11 +0900

    [starryeye, 2023.09.05] - 이커머스 admin project init
    내용
    - 이커머스 admin project init

---
+++ admin/src/test/java/dev/practice/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
